00fe7e4e418f176ea86829d2783b461f1483b0f1
John Doe <johndoe@example.com01~>
2025-04-29 16:21:54 +0300

    z

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import './Components/styleLogIn.css';
-import './Components/styleRegister.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { LogIn } from './Components/logIn';
 import { Register } from './Components/register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './Components/styleLogIn.css';
+import './Components/styleRegister.css';
 
 function App() {
   return (
-    <div>
-
-      <LogIn />
-      {/* <Register /> */}
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/Components/register" element={<Register />} />
+        {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        <Route path="/" element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<LogIn />} />
-//         <Route path="register" element={<Register />} />
-//       </Routes>
-
-//       <div>
-//         <p>Lorem import</p>
-//         {/* <LogIn /> */}
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
 export default App;
--- src/Components/logIn.js
+++ src/Components/logIn.js
@@ -1,67 +1,64 @@
-import React from "react";
-import Link from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function LogIn() {
     return (
         <div id="wrappLogIn">
-
-            <nav>
-
-            </nav>
+            <nav></nav>
 
             <div id="left">
-                <button id="backBnt"> <img src="backBtn.png" alt=""></img></button>
+                <button id="backBnt">
+                    <img src="backBtn.png" alt="Back" />
+                </button>
                 <div id="divForH1">
-                    <h1 id="title">заповніть вхід до облікового запису</h1>
+                    <h1 id="title">Заповніть вхід до облікового запису</h1>
                 </div>
             </div>
 
             <div id="right">
-                <form class="main">
-
-                    <label class="inp">Електронна пошта або мобільний номер:</label> <br />
-                    <input type="text" class="first" /><br />
+                <form className="main">
+                    <label className="inp">Електронна пошта або мобільний номер:</label> <br />
+                    <input type="text" className="first" /><br />
                     <div id="divForText1">
-                        <p class="text">By entering your mobile number and one-time code sign-in option, <br />
-                            you agree to receive a one-time verification coe via SMS from IKEA. <br />
-                            Message and data rates may apply.</p>
+                        <p className="text">
+                            By entering your mobile number and one-time code sign-in option, <br />
+                            you agree to receive a one-time verification code via SMS from IKEA. <br />
+                            Message and data rates may apply.
+                        </p>
                     </div>
-                    <p class="text2">More info about Private Policy</p>
-                    <label class="inp">Пароль:</label> <br />
-
+                    <p className="text2">More info about Private Policy</p>
+                    <label className="inp">Пароль:</label> <br />
 
                     <div id="pass">
                         <input type="password" id="inputPass" /><br />
                         <button id="buttonPass">
-                            <img src="/Vector.png" alt="23" id="ies" />
+                            <img src="/Vector.png" alt="Show password" id="ies" />
                         </button>
                     </div>
 
-
-
                     <div id="divForBtnForgotPass">
                         <a id="butforgotPass">Забули свій пароль?</a>
+                        {/* <Link to="/forgot-password" id="butforgotPass">Забули свій пароль?</Link> */}
                     </div>
                     <br />
                     <div id="divForSaveInfo">
                         <div id="divForCheckbox">
-                            <input type="checkbox" value='1' id="saveInfo" />
+                            <input type="checkbox" value="1" id="saveInfo" />
                         </div>
                         <div id="divForTextSaveInfo">
                             <p>Зберегти інформацію</p>
                         </div>
                         <div id="divForInfo">
-                            <img src="/info.png" />
+                            <img src="/info.png" alt="Info" />
                         </div>
                     </div>
                     <br />
 
-                    <button class="Akk" href="#">Продовжити</button>
+                    <button className="Akk">Продовжити</button>
 
-                    <p class="pMa">У вас це не має облікового запису? Створіть його:</p>
-                    {/* <Link to='/register'>Створити аккаунт</Link> */}
-                    <button className="Ma">Створити аккаунт</button>
-
+                    <div className='Ma'>
+                        <Link to="/register"><p id='pInLink'>Створити аккаунт</p></Link>
+                    </div>
                 </form>
             </div>
         </div>
