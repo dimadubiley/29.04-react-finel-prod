4bb103d436d0bac72901ee9e85e16f75e6cb7417
Dima <Dima@gmail.com>
2025-05-13 10:43:40 +0300

    ddd

--- src/Components/design.js
+++ src/Components/design.js
@@ -6,34 +6,36 @@ import './styleDesing.css';
 export function Desing() {
     return (
         <div id='wrappDesing'>
-            <div id='heroImg'>
-                <h1 id='titleDesing'>Створи дизайн своєї мрії</h1>
-                <div id='divForThreeCards'>
-                    <div className='card'>
-                        <img src='photo for first card desing.png' alt='' className='imgForCard' />
-                        <hr className='hrForCard' />
-                        <h4 className='miniH4ForCard'>створи дизайн</h4>
-                        <p className='textFor Card'>Habitasse consequat egestas lectus blandit </p>
+            <div id='divForBgImg'>
+                <div id='heroImg'>
+                    <h1 id='titleDesing'>Створи дизайн своєї мрії</h1>
+                    <div id='divForThreeCards'>
+                        <div className='card'>
+                            <img src='photo for first card desing.png' alt='' className='imgForCard' />
+                            <hr className='hrForCard' />
+                            <h4 className='miniH4ForCard'>створи дизайн</h4>
+                            <p className='textFor Card'>Habitasse consequat egestas lectus blandit </p>
+                        </div>
+                        <div className='card'>
+                            <img src='photo for second card desing.png' alt='' className='imgForCard' />
+                            <hr className='hrForCard' />
+                            <h4 className='miniH4ForCard'>Комбінуй ідеї</h4>
+                            <p className='textFor Card'>Habitasse consequat egestas </p>
+                        </div>
+                        <div className='card'>
+                            <img src='photo for third card desing.png' alt='' className='imgForCard' />
+                            <hr className='hrForCard' />
+                            <h4 className='miniH4ForCard'>результат</h4>
+                            <p className='textFor Card'>Habitasse tus consequat egestas lectus blandit tus </p>
+                        </div>
                     </div>
-                    <div className='card'>
-                        <img src='photo for second card desing.png' alt='' className='imgForCard' />
-                        <hr className='hrForCard' />
-                        <h4 className='miniH4ForCard'>Комбінуй ідеї</h4>
-                        <p className='textFor Card'>Habitasse consequat egestas </p>
-                    </div>
-                    <div className='card'>
-                        <img src='photo for third card desing.png' alt='' className='imgForCard' />
-                        <hr className='hrForCard' />
-                        <h4 className='miniH4ForCard'>результат</h4>
-                        <p className='textFor Card'>Habitasse tus consequat egestas lectus blandit tus </p>
-                    </div>
-                </div>
 
-                <div id='boddomHeroImh'>
-                    <h1>втілюй свої ідеї</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur. Habitasse consequat egestas lectus blandit consequat. A sed placerat sapien amet scelerisque ullamcorper amet. Elementum consequat risus nullam </p>
-                </div>
+                    <div id='boddomHeroImh'>
+                        <h1>втілюй свої ідеї</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur. Habitasse consequat egestas lectus blandit consequat. A sed placerat sapien amet scelerisque ullamcorper amet. Elementum consequat risus nullam </p>
+                    </div>
 
+                </div>
             </div>
 
             <div id='firstSec'>
