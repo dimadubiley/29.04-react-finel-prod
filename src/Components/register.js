62b31a8cefec921909eb364e955962950f280e8e
John Doe <johndoe@example.com01~>
2025-05-14 13:54:25 +0300

    register: add -DD

--- src/Components/register.js
+++ src/Components/register.js
@@ -3,65 +3,65 @@ import './styleRegister.css';
 
 export function Register() {
     return (
-        <div id="wrappRegister">
-            <div id="left">
+        <div id="wrappRegister-DD">
+            <div id="left-DD">
                 <button id="backBnt"> <img src="backBtn.png" alt=""></img></button>
             </div>
 
-            <div id="right">
-                <form class="main">
+            <div id="right-DD">
+                <form class="main-DD">
 
-                    <div className="divForInfoRegister" id="divName">
+                    <div className="divForInfoRegister-DD" id="divName-DD">
                         <label class="inp">Ім'я</label> <br />
-                        <input type="text" class="inputInfo" /><br />
+                        <input type="text" class="inputInfo-DD" /><br />
                     </div>
 
-                    <div className="divForInfoRegister">
+                    <div className="divForInfoRegister-DD">
                         <label class="inp">Фамілія</label> <br />
-                        <input type="text" class="inputInfo" /><br />
+                        <input type="text" class="inputInfo-DD" /><br />
                     </div>
-                    <div className="divForInfoRegister">
+                    <div className="divForInfoRegister-DD">
                         <label class="inp">Дата народження</label> <br />
-                        <input type="date" class="inputInfo" /><br />
+                        <input type="date" class="inputInfo-DD" /><br />
                     </div>
-                    <div className="divForInfoRegister">
+                    <div className="divForInfoRegister-DD">
                         <label class="inp">Країна</label> <br />
-                        <input type="text" class="inputInfo" /><br />
+                        <input type="text" class="inputInfo-DD" /><br />
                     </div>
-                    <div className="divForInfoRegister">
+                    <div className="divForInfoRegister-DD">
                         <label class="inp">Адреса</label> <br />
-                        <input type="text" class="inputInfo" /><br />
+                        <input type="text" class="inputInfo-DD" /><br />
                     </div>
-                    <div className="divForInfoRegister">
+                    <div className="divForInfoRegister-DD">
                         <label class="inp">мобільний номер</label> <br />
-                        <input type="text" class="inputInfo" /><br />
+                        <input type="text" class="inputInfo-DD" /><br />
                     </div>
                     <br />
-                    <p class="text">By entering your mobile number and one-time code sign-in option, <br />
+                    <p class="text-DD">By entering your mobile number and one-time code sign-in option, <br />
                         you agree to receive a one-time verification coe via SMS from IKEA. <br />
                         Message and data rates may apply.</p>
-                    <p class="text2">More info about Private Policy</p>
-                    <div className="divForInfoRegister">
-                        <label class="inp">Електронна пошта або мобільний номер:</label> <br />
-                        <input type="text" class="first" /><br />
+                    <p class="text2-DD">More info about Private Policy</p>
+                    <div className="divForInfoRegister-DD">
+                        <label class="inp-DD">Електронна пошта або мобільний номер:</label> <br />
+                        <input type="text" class="first-DD" /><br />
                     </div>
-                    <div className="divForInfoRegister" id="divForPass">
-                        <label class="inp">Пароль:</label> <br />
-                        <div id="pass">
-                            <input type="password" id="inputPass" /><br />
+                    <div className="divForInfoRegister-DD" id="divForPass-DD">
+                        <label class="inp-DD">Пароль:</label> <br />
+                        <div id="pass-DD">
+                            <input type="password" id="inputPass-DD" /><br />
                         </div>
                     </div>
-                    <div id="divForAgree">
-                        <div id="divForCheckboxSaveInfo">
-                            <input type="checkbox" value='1' id="saveInfo" />
+                    <div id="divForAgree-DD">
+                        <div id="divForCheckboxSaveInfo-DD">
+                            <input type="checkbox" value='1' id="saveInfo-DD" />
                         </div>
-                        <div id="divForTextIAgree">
-                            <p id="iAgree">Я, ознайомлений з Політикою конфіденційності з обробки та захисту персональних даних</p>
+                        <div id="divForTextIAgree-DD">
+                            <p id="iAgree-DD">Я, ознайомлений з Політикою конфіденційності з обробки та захисту персональних даних</p>
                         </div>
                     </div>
                     <br />
 
-                    <button class="Akk" href="#">Продовжити</button>
+                    <button class="Akk-DD" href="#">Продовжити</button>
 
                 </form>
             </div>
